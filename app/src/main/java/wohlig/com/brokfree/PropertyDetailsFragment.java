2f80accca1df3248924d300be9262092d4bcc4e0
kothavaleganesh <ganesh.wohlig@gmail.com>
2016-07-27 19:09:09 +0530

    brokfree1

--- app/src/main/java/wohlig/com/brokfree/PropertyDetailsFragment.java
+++ app/src/main/java/wohlig/com/brokfree/PropertyDetailsFragment.java
@@ -4,6 +4,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +15,8 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 /**
  * Created by Mahesh on 6/30/2016.
@@ -100,6 +103,16 @@ public class PropertyDetailsFragment extends AppCompatActivity {
         propertymap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String uri = String.format(Locale.ENGLISH, "geo:%f,%f",  19.059867, 72.850132);
+                try{
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                    intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                    startActivity(intent);}
+                catch(Exception e){
+                    Intent i = new Intent(android.content.Intent.ACTION_VIEW);
+                    i.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.google.android.apps.maps"));
+                    startActivity(i);
+                }
 
 
 
@@ -187,8 +200,8 @@ public class PropertyDetailsFragment extends AppCompatActivity {
             public void onClick(View v) {
                 FragmentManager fragmentManager = getFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                FeedbackFragment feedbackFragment = new FeedbackFragment();
-                fragmentTransaction.replace(R.id.container, feedbackFragment);
+                Test test = new Test();
+                fragmentTransaction.replace(R.id.container, test);
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
             }
--- app/src/main/java/wohlig/com/brokfree/Test.java
+++ app/src/main/java/wohlig/com/brokfree/Test.java
@@ -1,15 +1,143 @@
 package wohlig.com.brokfree;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 /**
  * Created by Mahesh on 7/8/2016.
  */
-public class Test extends AppCompatActivity {
+public class Test extends Fragment {
+    Spinner spinner1, spinner2, spinner3, spinner4, spinner5, spinner6, spinner, spinner7;
+    TextView amen;
+    LinearLayout floor, amen1, car, vastu, locality, loan, sell, payment;
+    CardView cardView, cardView1, cardView2, cardView3, cardView4, cardView5, cardView6, cardView7;
+    Button submit;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_other_requirements);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_other_requirements, container, false);
+        ((PropertyDetailsFragment)this.getActivity()).setToolbarText("Requirements");
+        String[] items = new String[]{"Inevitable", "important", "doesnt matter"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, items);
+
+        spinner1 = (Spinner) view.findViewById(R.id.spinner1);
+        spinner1.setAdapter(adapter);
+        spinner2 = (Spinner) view.findViewById(R.id.spinner2);
+        spinner2.setAdapter(adapter);
+        spinner3 = (Spinner) view.findViewById(R.id.spinner3);
+        spinner3.setAdapter(adapter);
+        spinner4 = (Spinner) view.findViewById(R.id.spinner4);
+        spinner4.setAdapter(adapter);
+        spinner5 = (Spinner) view.findViewById(R.id.spinner5);
+        spinner5.setAdapter(adapter);
+        spinner6 = (Spinner) view.findViewById(R.id.spinner6);
+        spinner6.setAdapter(adapter);
+        spinner = (Spinner) view.findViewById(R.id.spinner);
+        String[] items1 = new String[]{"posh", "good", "any"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, items1);
+        spinner.setAdapter(adapter1);
+        spinner7 = (Spinner) view.findViewById(R.id.spinner7);
+        spinner7.setAdapter(adapter);
+        amen1 = (LinearLayout) view.findViewById(R.id.amen_1);
+        amen = (TextView) view.findViewById(R.id.amen);
+        cardView = (CardView) view.findViewById(R.id.card_view1);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                amen1.setVisibility(amen1.isShown() ? View.GONE : View.VISIBLE);
+            }
+        });
+        floor = (LinearLayout) view.findViewById(R.id.floor);
+        cardView1 = (CardView) view.findViewById(R.id.card_view);
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                floor.setVisibility(floor.isShown() ? View.GONE : View.VISIBLE);
+
+            }
+        });
+        vastu = (LinearLayout) view.findViewById(R.id.vastu);
+        cardView2 = (CardView) view.findViewById(R.id.card_view2);
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vastu.setVisibility(vastu.isShown() ? View.GONE : View.VISIBLE);
+
+            }
+        });
+
+        locality = (LinearLayout) view.findViewById(R.id.locality);
+        cardView3 = (CardView) view.findViewById(R.id.card_view3);
+        cardView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locality.setVisibility(locality.isShown() ? View.GONE : View.VISIBLE);
+
+            }
+        });
+
+        car = (LinearLayout) view.findViewById(R.id.car);
+        cardView4 = (CardView) view.findViewById(R.id.card_view4);
+        cardView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                car.setVisibility(car.isShown() ? View.GONE : View.VISIBLE);
+
+            }
+        });
+        loan = (LinearLayout) view.findViewById(R.id.loan);
+        cardView5 = (CardView) view.findViewById(R.id.card_view5);
+        cardView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loan.setVisibility(loan.isShown() ? View.GONE : View.VISIBLE);
+
+            }
+        });
+        sell = (LinearLayout) view.findViewById(R.id.sell);
+        cardView6 = (CardView) view.findViewById(R.id.card_view6);
+        cardView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sell.setVisibility(sell.isShown() ? View.GONE : View.VISIBLE);
+
+            }
+        });
+        payment = (LinearLayout) view.findViewById(R.id.payment);
+        cardView7 = (CardView) view.findViewById(R.id.card_view7);
+        cardView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                payment.setVisibility(payment.isShown() ? View.GONE : View.VISIBLE);
+
+            }
+        });
+        submit = (Button) view.findViewById(R.id.brequiemnts_submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                FeedbackFragment feedbackFragment = new FeedbackFragment();
+                fragmentTransaction.replace(R.id.container, feedbackFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return view;
     }
+
+
 }
